ddd880d551a4e0735b9803401f2d037700f2a8fb
26TQT01-TiffanyQuex <tiffanyquexdb26@gmail.com>
2026-06-16 17:18:31 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("DATOS ACADÉMICOS\nNombre: Tiffany Godoy" + "\nGrado : IV C" +
+           "\nClave: 12");
+
+        string[] color = new string[9];
+        color[1] = "café";
+        color[2] = "lila";
+        color[3] = "rojo";
+        color[4] = "naranja";
+        color[5] = "amarillo";
+        color[6] = "verde";
+        color[7] = "morado";
+        color[8] = "negro";
+
+        Console.WriteLine("ingrese un número del 1 al 8");
+        int i = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("color: " + color[i-1]);
+    }
+}
