90acecb9747b863acede5b177c8533e51ece847b
26TQT01-TiffanyQuex <tiffanyquexdb26@gmail.com>
2026-06-15 18:15:03 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int comparador;
+        bool ExisteRepetido= false;
+        int[] numerosEnteros = new int[15];
+
+        for (int indice = 0; indice < numerosEnteros.Length; indice++)
+        {
+            Console.WriteLine("Digite el número "+(indice+1)+":");
+            numerosEnteros[indice] = Convert.ToInt32(Console.ReadLine());
+        }
+        for (int indice = 0; indice < numerosEnteros.Length; indice++)
+        {
+            for (comparador = indice + 1; comparador < 15; comparador++)
+            {
+                if (numerosEnteros[indice] == numerosEnteros[comparador]);
+                {
+                    ExisteRepetido = true;
+                }
+            }
+
+        }
+        if (ExisteRepetido = true)
+        {
+            Console.WriteLine("Sí existe al menos un repetido");
+        }
+        else
+        {
+            Console.WriteLine("No esxiste ningún repetido");
+        }
+    }
+}
